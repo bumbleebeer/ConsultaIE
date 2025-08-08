ed7288ae2a157eaa34fe7222e210252b1887ca8a
FelipeAzeredo <felipeats2003@gmail.com>
2025-08-07 23:37:04 -0300

    Andamento consulta cnpj

--- consulta.js
+++ consulta.js
@@ -8,9 +8,8 @@ async function buscarCEP(cep) {
       return;
     }
 
-    const tabela = document.getElementById("resultado");
+    const tabela = document.getElementById("resultadocep");
 
-    // Cria uma nova linha
     const linha = document.createElement("tr");
 
     linha.innerHTML = `
@@ -22,7 +21,6 @@ async function buscarCEP(cep) {
       <td>${dados.uf}</td>
     `;
 
-    // Adiciona a linha no final da tabela (mantendo as anteriores)
     tabela.appendChild(linha);
 
   } catch (erro) {
@@ -30,9 +28,58 @@ async function buscarCEP(cep) {
   }
 }
 
-document.getElementById("btn").onclick = () => {
+document.getElementById("btn_cep").onclick = () => {
   const cep = document.getElementById("cep").value.trim();
   if (cep) {
     buscarCEP(cep);
+  }
+};
+
+async function buscarCNPJ(cnpj) {
+  try {
+    const resposta = await fetch(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`);
+    const dados = await resposta.json();
+
+    if (dados.erro) {
+      alert("CNPJ não encontrado!");
+      return;
+    }
+
+    const tabela = document.getElementById("resultadocnpj");
+
+    const linha = document.createElement("tr");
+
+    linha.innerHTML = `
+      <td>${dados.situacao}</td>
+      <td>${dados.porte}</td>
+      <td>${dados.cnpj}</td>
+      <td>${dados.nome}</td>
+      <td>${dados.fantasia}</td>
+      <td>${dados.atividade_principal[0].text}</td>
+      <td>${dados.atividades_secundarias.map(a => a.text).join('; ')}</td>
+      <td>${dados.logradouro}</td>
+      <td>${dados.numero}</td>
+      <td>${dados.complemento}</td>
+      <td>${dados.bairro}</td>
+      <td>${dados.municipio}</td>
+      <td>${dados.cep}</td>
+      <td>${dados.telefone}</td>
+      <td>${dados.email}</td>
+      <td>${dados.uf}</td>
+      <td>${dados.abertura}</td>
+      <td>${dados.data_situacao}</td>
+    `;
+
+    tabela.appendChild(linha);
+
+  } catch (erro) {
+    console.error("Erro na consulta", erro);
+  }
+}
+
+document.getElementById("btn_cnpj").onclick = () => {
+  const cnpj = document.getElementById("cnpj").value.trim();
+  if (cnpj) {
+    buscarCNPJ(cnpj);
   }
 };
